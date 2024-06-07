c2762165e6f7b558b2a9b8e0acf7807cc6a8356c
Akhtar Muhammad Achsan <pesonaponasa@gmail.com>
2024-06-07 19:02:08 +0700

    Documentation
    Added comments on code to help readability

--- AlgoritmaDP.py
+++ AlgoritmaDP.py
@@ -3,46 +3,81 @@ import tracemalloc
 
 
 def unbounded_knapsack(n, W1, W2, W3, weights, profits):
+    """
+    Weight = limit atau batas dari knapsack
+    Cost = harga jika barang diambil
+    Profit = total profit yang diapat jika barang diambil
+    """
+
     start_time = time.time()
     tracemalloc.start()
 
     #Top-Down
     memo = {}
 
+    #Calculate value pada tabel
     def dp(w1, w2, w3):
+        #Return value jika sudah pernah dihitung
         if (w1, w2, w3) in memo:
             return memo[(w1, w2, w3)]
+        
         max_profit = 0
+        #Buat tabel dynamic programming berdasarkan top-down
+        #Looping pada setiap item
         for i in range(n):
             w1_i, w2_i, w3_i = weights[i]
             p1_i, p2_i = profits[i]
+            #JIka cost lebih kecil dari total weight maka
             if w1 >= w1_i and w2 >= w2_i and w3 >= w3_i:
+                #Hitung profit secara recursive
+                #Rumus profit saat barang ke i diambil
+                #Rumus profit p1i + p2i + F (i− w1, j − w2, k, -w3)
                 profit = dp(w1 - w1_i, w2 - w2_i, w3 - w3_i) + p1_i + p2_i
+                #Ambil max dari max_profit atau profit
+                #Rumus lain saat barang ke i tidak diambil
+                #Bentuk lain F (i − 1, j)
                 max_profit = max(max_profit, profit)
+        
+        #Simpan pada array memo dengan lokasi berdasarkan weight
         memo[(w1, w2, w3)] = max_profit
         return max_profit
 
+    #Profit maksimum saat total weight awal
     max_profit = dp(W1, W2, W3)
     
     # Backtrack to find the items taken
     taken_items = []
     current_w1, current_w2, current_w3 = W1, W2, W3
+    #Looping selama weight tidak 0
     while current_w1 > 0 and current_w2 > 0 and current_w3 > 0:
         found = False
+        #Looping cek untuk setiap item
         for i in range(n):
             w1_i, w2_i, w3_i = weights[i]
             p1_i, p2_i = profits[i]
+            #JIka masik cukup maka
             if current_w1 >= w1_i and current_w2 >= w2_i and current_w3 >= w3_i:
+                #Cek apakah barang terambil atau tidak
+                #Barang terambil apabila value saat ini adalah jumlah dari value sebelumnya + profit barang
                 if dp(current_w1,current_w2,current_w3) == dp(current_w1 - w1_i,current_w2 - w2_i,current_w3 - w3_i) + p1_i + p2_i:
+                    #JIka barang diambil maka simpan index
                     taken_items.append(i)
+                    #Kurangi weight dengan cost item untuk pengecekan selanjutnya
                     current_w1 -= w1_i
                     current_w2 -= w2_i
                     current_w3 -= w3_i
+                    #Jika terambil maka tidak perlu cek barang lain
                     found = True
                     break
+                
+        #Jika tidak ada lagi item yang dapat dicari maka stop looping
         if not found:
             break
 
+    _, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    return max_profit, taken_items, (time.time() - start_time)*10**3, peak
+    
     """
     # Initialize the DP table
     dp = [[[0 for _ in range(W3 + 1)] for _ in range(W2 + 1)] for _ in range(W1 + 1)]
@@ -80,9 +115,7 @@ def unbounded_knapsack(n, W1, W2, W3, weights, profits):
         if not found:
             break
     """
-    _, peak = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-    return max_profit, taken_items, (time.time() - start_time)*10**3, peak
+    
 
 def calculateCost(wood, stone, steel):
     rWood = 1
--- AlgoritmaGreedy.py
+++ AlgoritmaGreedy.py
@@ -1,6 +1,7 @@
 import time
 import tracemalloc
 
+#Function untuk menghiitung total cost dari ite,
 def calculateCost(wood, stone, steel):
     rWood = 1
     rStone = 1.5
@@ -8,21 +9,32 @@ def calculateCost(wood, stone, steel):
 
     return wood*rWood + stone*rStone + steel*rSteel
 
+#Function untuk menghitung total profit berdasarkan rasio yang dimiliki
 def calculateProfit(production, population, rProd, rPop):
     return production * rProd + population * rPop
 
+#Function algoritma greedy by weight
 def greed_byWeight(weights, values, capacity):
     start_time = time.time()
     tracemalloc.start()
 
     taken = []
+    #Hitung semua cost dari item yang ada dan masukkan pada array
     convWeights = [calculateCost(weights[i][0],weights[i][1], weights[i][2]) for i in range(len(weights))]
+    #Sort array cost secara ascending
+    #Sort secara ascending untuk mendapat cost min
     sortedW = [calculateCost(weights[i][0],weights[i][1], weights[i][2]) for i in range(len(weights))]
     sortedW.sort()
+
     curr_capx, curr_capy, curr_capz = capacity
+    #Untuk semua item pada array yang telah di sort
+    #Loop dari terkecil hingga terbesar
     for i in sortedW:
+        #Ambil index asli dari item ke i
         idxmin = convWeights.index(i)
+        #Ambil item hingga knapsack tidak cukup
         while curr_capx >= weights[idxmin][0] and curr_capy >= weights[idxmin][1] and curr_capz >= weights[idxmin][2]:
+            #Simpan index asli item pada array taken
             taken.append(idxmin)
             curr_capx -= weights[idxmin][0]
             curr_capy -= weights[idxmin][1]
@@ -31,21 +43,36 @@ def greed_byWeight(weights, values, capacity):
     _, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
     print(time.time() - start_time)
+    #Return hasil greedy
     return taken, (time.time() - start_time)*10**3, peak
 
+#Function greedy by density
 def greed_byDensity(weights, values, capacity, ratio):
     start_time = time.time()
     tracemalloc.start()
+
     taken = []
+    #Hitung profit dari setiap item yang ada
     convValues= [calculateProfit(values[i][0], values[i][1], ratio[0], ratio[1]) for i in range(len(values))]
+    #Hitung cost dari setiap item yang ada
     convWeights = [calculateCost(weights[i][0],weights[i][1], weights[i][2]) for i in range(len(weights))]
+    #Hitung density berdasarkan profit/cost
     convDensity = [convValues[i]/convWeights[i] for i in range(len(weights))]
+
+    #Sort array yang berisi density item secara descending
+    #Sort secara descending untuk mendapat density max
     sortedD = [convValues[i]/convWeights[i] for i in range(len(weights))]
     sortedD.sort(reverse=True)
+
     curr_capx, curr_capy, curr_capz = capacity
+    #Untuk semua item pada array yang telah di sort
+    #Loop dari density terbesar hingga terkecil
     for i in sortedD:
+        #Ambil index asli dari item i
         idxmax = convDensity.index(i)
+        #Ambil item hingga knapsack tidak cukup
         while curr_capx >= weights[idxmax][0] and curr_capy >= weights[idxmax][1] and curr_capz >= weights[idxmax][2]:
+            #Simpan index asli item pada array
             taken.append(idxmax)
             curr_capx -= weights[idxmax][0]
             curr_capy -= weights[idxmax][1]
@@ -53,20 +80,30 @@ def greed_byDensity(weights, values, capacity, ratio):
     
     _, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
+    #Return hasil barang yang diambil
     return taken, (time.time() - start_time)*10**3, peak
 
+#Function algoritma greedy by profit
 def greed_byProfit(weights, values, capacity, ratio):
     start_time = time.time()
     tracemalloc.start()
 
     taken = []
+    #Hitung profit dari setiap item yang ada
     convValues= [calculateProfit(values[i][0], values[i][1], ratio[0], ratio[1]) for i in range(len(values))]
+    #Sort array yang berisi profit item secara descending
+    #Sort secara descending untuk mendapat profit max
     sortedV = [calculateProfit(values[i][0], values[i][1], ratio[0], ratio[1]) for i in range(len(values))]
     sortedV.sort(reverse=True)
     curr_capx, curr_capy, curr_capz = capacity
+    #Untuk semua item pada array yang telah di sort
+    #Loop dari profit terbesar hingga terkecil
     for i in sortedV:
+        #Ambil index asli dari item i
         idxmax = convValues.index(i)
+        #Ambil item hingga knapsack tidak cukup
         while curr_capx >= weights[idxmax][0] and curr_capy >= weights[idxmax][1] and curr_capz >= weights[idxmax][2]:
+            #Simpan index asli item pada array
             taken.append(idxmax)
             curr_capx -= weights[idxmax][0]
             curr_capy -= weights[idxmax][1]
@@ -74,4 +111,5 @@ def greed_byProfit(weights, values, capacity, ratio):
     
     _, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
+     #Return hasil barang yang diambil
     return taken, (time.time() - start_time)*10**3, peak
